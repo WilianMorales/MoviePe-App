7c81a6a8936e773bdbacbea89e797f2325eb625a
WilianMorales <willian.moralesch@gmail.com>
2022-12-10 01:02:09 -0500

    Implementation of the project structure.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieComponent } from './pages/movie/movie.component';
+import { SearchComponent } from './pages/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'search/:texto',
+    component: SearchComponent
+  },
+  {
+    path: 'movie/:id',
+    component: MovieComponent
+  },
+  {
+    path: '**', redirectTo: '/home', pathMatch: 'full'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
